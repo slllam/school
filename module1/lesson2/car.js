63631b3cc376410d39dd556aa1fef99ef3ebfe25
slllam <108426500+slllam@users.noreply.github.com>
2022-07-09 15:52:09 +0500

    Add files via upload

---
+++ module1/lesson2/car.js
@@ -0,0 +1,24 @@
+// JavaScript-код с созданием переменных
+/* Автор:
+Чернышев Юрий */
+
+const carName = ('Porsche 911 Turbo S');
+let maxCarSpeed = ('330');
+let carOwner = ('Happy owner');
+
+alert (carName);
+alert (maxCarSpeed);
+alert (carOwner);
+
+maxCarSpeed = 331;
+console.log("У машины изменилась скорость", maxCarSpeed);
+carOwner = 'Happy owner #2';
+console.log("У машины изменился владелец", carOwner);
+maxCarSpeed = 332;
+console.log("У машины изменилась скорость", maxCarSpeed);
+carOwner = 'Happy owner #3';
+console.log("У машины изменился владелец", carOwner);
+maxCarSpeed = 333;
+console.log("У машины изменилась скорость", maxCarSpeed);
+carOwner = 'Happy owner #4';
+console.log("У машины изменился владелец", carOwner);
---
+++ module1/lesson2/index.js
@@ -0,0 +1,15 @@
+// JavaScript-код с созданием переменных
+/* Автор:
+Чернышев Юрий */
+
+const myName = 'Юрий';
+const mySurname = 'Чернышев';
+let myFavoriteDrink = 'Coffee';
+let myFavoriteAnimal = 'Cats';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
