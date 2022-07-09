fc719f0b74e1fa73f68c2aacd9cd26c9a15bc947
slllam <108426500+slllam@users.noreply.github.com>
2022-07-09 14:50:28 +0500

    Add files via upload

---
+++ module1/lesson1/first-project/js-info.js
@@ -0,0 +1,12 @@
+/* Кто разработал язык программирования JavaScript?
+За сколько дней был разработан JavaScript?
+В каком году появился JavaScript?
+JavaScript - это то же самое, что и Java? (отвечай либо “JavaScript - это Java”, либо “JavaScript - это не Java”)
+Как называется спецификация JavaScript? 
+*/
+
+alert('JavaScript разработал Брендан Эйх');
+alert('JavaScript был разработан за 10 дней');
+alert('JavaScript появился в 1995 году');
+alert('JavaScript - это не Java');
+alert('Спецификация называется ECMAScript');
