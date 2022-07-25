59f1fdfa0091d86828003cd43092c8f27498b171
Yuriy <108426500+slllam@users.noreply.github.com>
2022-07-25 23:37:39 +0500

    Update z-5.js

--- module1/lesson10/z-5.js
+++ module1/lesson10/z-5.js
@@ -1,2 +1,9 @@
 // Lesson 10
 
+function sum(...numbers) {  // при помощи оператора rest все полученные значения преобразуем в массив
+   return numbers.reduce((previous, current) => previous + current);
+  }
+  
+  console.log(sum(2, 3)); // 5
+  console.log(sum(1, 2, 3)); // 6
+  console.log(sum(10, 15, 249, 653, 846)); // 1773
