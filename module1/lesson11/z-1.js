928bf19ce2d7e07a928a55c1bd06b32cce5b0743
Yuriy <108426500+slllam@users.noreply.github.com>
2022-07-25 11:21:44 +0500

    Add files via upload

--- module1/lesson11/z-1.js
+++ module1/lesson11/z-1.js
@@ -1,15 +1,13 @@
 // Lesson 11 - Вывести дату в формате: 05.05.2001
 
-// const date = new Date();
-const date = new Date('31 December, 2022');
+ const date = new Date();
+//const date = new Date('31 December, 2022');
 const separator = '-';
 
 function getDateFormat(date, separator) {
 	const yyyy = date.getFullYear();
 	let mm = date.getMonth() + 1;
-	if (mm < 10) {
-		mm = '0' + mm;
-	}
+	mm = ((mm < 10) ? `0${mm}` : mm); //дописываем ведущий 0
 	const dd = date.getDate();	
 return result = String(dd + separator + mm + separator + yyyy); 
 };
