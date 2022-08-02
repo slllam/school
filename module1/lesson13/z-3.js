ad0470d6dcea669e2845a2de9cef8b84cfdddbcd
Yuriy <108426500+slllam@users.noreply.github.com>
2022-08-02 23:25:48 +0500

    Add files via upload

--- module1/lesson13/z-3.js
+++ module1/lesson13/z-3.js
@@ -1,7 +1,5 @@
 // Lesson 13
 
-const wordList = [];  // это массив, в котором хранятся все слова, которые есть в словаре
-
 class Dictionary {
   constructor (name) {
     this.name = name;
@@ -9,55 +7,35 @@ class Dictionary {
   }
     add(word,description){
       const dynamicKey = word;
-      if (!wordList.includes(word)) {
-        wordList.push(word);
- //       console.log(wordList);
-          this.words[dynamicKey] = {
-            word: word,
-            description: description,
+      if (dynamicKey in this.words) return;
+      else {
+        this.words[dynamicKey] = {
+        word: word,
+        description: description,
         };
       }
-    }
+    }  
     remove(word){
       const dynamicKey = word;
-      if (wordList.includes(word)) {  
+      if (dynamicKey in this.words) {
         delete this.words[dynamicKey];
-        const indexToDelete = wordList.indexOf(dynamicKey);  // Удаляем слово из словаря dictionary
-        wordList.splice(indexToDelete, 1);  // Удаляем слово из массива wordList
       }     
     } 
-    get(word){
+    get(word){   //Из метода необходимо возвратить найденный объект слова по переданному параметру ключа.
       const dynamicKey = word;
       return this.words[dynamicKey];
     }
-    showAllWords(){
-      wordList.forEach(element => {
-        console.log(element); 
-      });      
+    showAllWords(){  //вывести все слова, которые есть в объекте words, в консоль в формате “word - description”
+      for (const dynamicKey in this.words) {
+        console.log(`${this.words[dynamicKey].word} - ${this.words[dynamicKey].description}`);  
+      }
     }
     
-
-
 }
 const dictionary = new Dictionary("Толковый словарь");
-console.log(dictionary);
+// console.log(dictionary);
 dictionary.add("JavaScript", "популярный язык программирования");
 dictionary.add("Веб-разработчик", "Человек, который создает новые сервисы и сайты или поддерживает и дополняет существующие");
 dictionary.add("JavaScript", "популярный язык программирования");
-//dictionary.remove("JavaScript");
-console.log(dictionary.get("JavaScript"));
-//console.log(result);
+dictionary.remove("JavaScript");
 dictionary.showAllWords();
-
-
-
-// const dictionary = new Dictionary("Толковый словарь");
-// dictionary.add("JavaScript", "популярный язык программирования");
-// dictionary.add(
-//   "Веб-разработчик",
-//   "Человек, который создает новые сервисы и сайты или поддерживает и дополняет существующие"
-// );
-// dictionary.remove("JavaScript");
-// dictionary.showAllWords();
-// Веб-разработчик - Человек, который создает новые сервисы и сайты или поддерживает
-// и дополняет существующие
