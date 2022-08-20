ae82ca146380bdee24dd0bb8a1e355b8fa148393
Yuriy <108426500+slllam@users.noreply.github.com>
2022-08-21 00:27:19 +0500

    Add files via upload

--- module1/lesson15/z2/promise.js
+++ module1/lesson15/z2/promise.js
@@ -22,21 +22,30 @@ const hideLoader = () => {
     getLoader.style.display = "none";
 };
 
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then((responce) => {
-        if (!responce.ok) {
-            throw new Error("Ошибочка вышла");
-        }
-        showLoader();
-        return responce.json();
-    })
-    .then((responce_data) => {
-        outputUsers(responce_data);
-    })
-    .catch((error) => {
-        console.log('Error:', error);
-    })
-    .finally(() => {
-      hideLoader();
-      console.log('Fetching finished');
+const getUsersByIds = (users) => {
+    let usersList = [];
+    users.forEach((element) => {
+        usersList = users.map(element => fetch(`https://jsonplaceholder.typicode.com/users/${element}`));
     });
+    Promise.all(usersList)
+        .then(responses => {
+            const results = [];
+            responses.forEach((element) => {
+                if (!element.ok) throw Error('Ошибка, не удается выполнить Fetch');
+                else results.push(element.json())
+            });
+            console.log(results);
+            return Promise.all(results);             
+        })
+        .then(results => {
+            console.log(results);
+            outputUsers(results);
+        })
+        .catch((error) => {console.log('Error:', error)})
+        .finally (() => {
+            hideLoader();
+            console.log('Fetching finished');            
+        });
+}
+
+getUsersByIds([5, 6, 2, 1]);
